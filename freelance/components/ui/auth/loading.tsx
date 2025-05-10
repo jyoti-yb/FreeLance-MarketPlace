9e93a21b180d1a62b69ef21882bcf9cd8268a426
Jyoti_yb <balaboinajyotiyadav@gmail.com>
2025-05-10 08:54:14 +0530

    Added Clerk Authentication

---
+++ freelance/components/ui/auth/loading.tsx
@@ -0,0 +1,9 @@
+// components/auth/loading.tsx
+export function Loading() {
+    return (
+      <div className="h-screen flex items-center justify-center text-xl">
+        Loading...
+      </div>
+    );
+  }
+  
